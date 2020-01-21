665aeb67bc19d5fd43d3f5f2fd2ac24cf9181af6
nwalzer <nswalzer@gmail.com>
2020-01-21 11:44:12 -0500

    Updated vars to let

--- Project1/main.js
+++ Project1/main.js
@@ -1,10 +1,10 @@
 function main() {
     // Retrieve <canvas> element
-    var canvas = document.getElementById('webgl');
+    let canvas = document.getElementById('webgl');
 
 
     // Get the rendering context for WebGL
-    var gl = WebGLUtils.setupWebGL(canvas);
+    let gl = WebGLUtils.setupWebGL(canvas);
     if (!gl)
     {
         console.log('Failed to get the rendering context for WebGL');
@@ -12,7 +12,7 @@ function main() {
     }
 
     // Get file input button and add event handler
-    var fileInput = document.getElementById("drawModeFile");
+    let fileInput = document.getElementById("drawModeFile");
     fileInput.addEventListener("change", draw);
 
     // Initialize shaders
@@ -22,35 +22,35 @@ function main() {
     //Set up the viewport
     gl.viewport( 0, 0, canvas.width, canvas.height );
 
-    var points = [];
+    let points = [];
     points.push(vec4(-0.5, -0.5, 0.0, 1.0));
     points.push(vec4(0.5, -0.5, 0.0, 1.0));
     points.push(vec4(0.0, 0.5, 0.0, 1.0));
 
-    var pBuffer = gl.createBuffer();
+    let pBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, pBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
 
-    var vPosition = gl.getAttribLocation(program, "vPosition");
+    let vPosition = gl.getAttribLocation(program, "vPosition");
     gl.enableVertexAttribArray(vPosition);
     gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
 
 
-    var colors = [];
+    let colors = [];
     colors.push(vec4(1.0, 0.5, 0.0, 1.0));
     colors.push(vec4(0.0, 1.0, 0.0, 1.0));
     colors.push(vec4(0.0, 0.0, 1.0, 1.0));
 
-    var cBuffer = gl.createBuffer();
+    let cBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
 
-    var vColor = gl.getAttribLocation(program, "vColor");
+    let vColor = gl.getAttribLocation(program, "vColor");
     gl.enableVertexAttribArray(vColor);
     gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
 
 
-    var vPointSize = gl.getUniformLocation(program, "vPointSize");
+    let vPointSize = gl.getUniformLocation(program, "vPointSize");
     gl.uniform1f(vPointSize,10.0)
 
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
@@ -60,7 +60,7 @@ function main() {
     gl.drawArrays(gl.TRIANGLES, 0, points.length);
 
     window.onkeypress = function(event) {
-        var key = event.key;
+        let key = event.key;
         switch(key){
             case 'a':
                 gl.clear(gl.COLOR_BUFFER_BIT);
@@ -79,24 +79,15 @@ function main() {
 }
 
 function draw(evt){
-    var files = evt.target.files; // FileList object
-
-    // Loop through the FileList and render image files as thumbnails.
-    for (var i = 0, f; f = files[i]; i++) {
-
-        var reader = new FileReader();
-
-        // Closure to capture the file information.
+    let files = evt.target.files; // FileList object
+    for (let i = 0, f; f = files[i]; i++) {
+        let reader = new FileReader();
         reader.onload = (function(theFile) {
             return function(e) {
-                // Render thumbnail.
                 console.log(atob(e.target.result.split("base64,")[1]))
-                //console.log(reader.readAsBinaryString(e.target.result))
             };
         })(f);
-
-        // Read in the image file as a data URL.
-        reader.readAsDataURL(f);
+        reader.readAsDataURL(f)
     }
 }
 
